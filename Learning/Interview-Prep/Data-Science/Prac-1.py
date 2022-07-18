328aa105a270e5453c47418c2db4e33ec0cc4a8a
RecStu14 <resources.study14@gmail.com>
2022-07-18 14:08:20 +0800

    Update Prac-1.py

--- Learning/Interview-Prep/Data-Science/Prac-1.py
+++ Learning/Interview-Prep/Data-Science/Prac-1.py
@@ -1,1 +1,21 @@
+# PRAC 1
 
+# In your choice of language, write a program that prints the numbers ranging from one to 50. But for multiples of three, print "Fizz" instead of the 
+# number, and for the multiples of five, print "Buzz." For numbers which are multiples of both three and five, print "FizzBuzz" 
+
+for i in range(1,50):
+    if (((i % 3) == 0) and (i % 5) == 0):
+        print('FizzBuzz')
+    elif ((i % 5) == 0):
+        print('Buzz')
+    elif ((i % 3) == 0):
+          print('Fizz')
+    else:
+        print(i)
+
+# LEARNING POINT:
+"""
+The order in which we write the if and elif statements really matters alot!
+If you put either %3 only or %5 only above the statement having 'and', then 'FizzBuzz' 
+would never be displayed.
+"""
